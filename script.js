d3e290225a403f5f47107381c49101e68ff3335e
cesar1215 <cenriquez@angloaleman.edu.mx>
2022-10-24 15:23:10 -0500

    firs commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//Crea un funcion areaTriangulo con 2 parametro base, altura
+/*Dentro de la funcion crea un variable resultado de tipo let que multiplique
+la base por la altura entre 2
+*/
+//Agrega un return a la variable resultado
+function areaTriangulo(base, altura){
+    let resultado = (base * altura);
+    return resultado;
+}
+
+
+//En la consola manda a llamar la funcion areaTringuale y escribe los arguementos 15, 4
+console.log(areaTriangulo(15, 4));
